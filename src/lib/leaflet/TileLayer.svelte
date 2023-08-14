af0c624c26fcea834a331797bd7a1ff92f6b1d3a
Chetan <chetan.bawankule@ecinfosolutions.com>
2023-08-14 11:39:00 +0530

    made the component to add tilelayer

---
+++ src/lib/leaflet/TileLayer.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { getContext } from 'svelte';
+	import { map_tiles } from './base_tilesets.js';
+
+	export const tile_name = 'osm';
+	export const key = '';
+	const map = getContext('map')();
+	let tile_data = map_tiles.filter((tile) => tile.name == tile_name);
+	const addTileLayer = async () => {
+		let L = await import('leaflet');
+		L.tileLayer(tile_data[0].value.uri, tile_data[0].value.options).addTo(map);
+	};
+</script>
+
+<div use:addTileLayer>
+	<slot />
+</div>
--- src/routes/conus/+page.svelte
+++ src/routes/conus/+page.svelte
@@ -4,6 +4,7 @@
 	import { onMount } from 'svelte';
 	import Popup from '$lib/leaflet/Popup.svelte';
 	import ToolTip from '$lib/leaflet/ToolTip.svelte';
+	import TileLayer from '$lib/leaflet/TileLayer.svelte';
 	let map;
 	let usstates = {
 		features: []
@@ -37,31 +38,33 @@
 
 <div class="conus">
 	<Leaflet bind:map height={'600px'}>
-		{#each usstates?.features as feature}
-			<GeoJson
-				geojson={feature}
-				fitBounds={false}
-				fillOpacity={parseInt(feature.properties.STATE) / 100}
-				on:click={stateClick}
-			>
-				<ToolTip>
-					{feature.properties.NAME}
-				</ToolTip>
-			</GeoJson>
-		{/each}
-		{#each counties?.features as county (county.properties.FIPS)}
-			<GeoJson
-				geojson={county}
-				fitBounds={false}
-				fillColor={'red'}
-				fillOpacity={parseInt(county.properties.CNTY_FIPS) / 200}
-				color={'red'}
-				weight="1"
-			>
-				<ToolTip sticky={true}>
-					{county.properties.NAME}
-				</ToolTip>
-			</GeoJson>
-		{/each}
+		<TileLayer tile_name="osm" key="">
+			{#each usstates?.features as feature}
+				<GeoJson
+					geojson={feature}
+					fitBounds={false}
+					fillOpacity={parseInt(feature.properties.STATE) / 100}
+					on:click={stateClick}
+				>
+					<ToolTip>
+						{feature.properties.NAME}
+					</ToolTip>
+				</GeoJson>
+			{/each}
+			{#each counties?.features as county (county.properties.FIPS)}
+				<GeoJson
+					geojson={county}
+					fitBounds={false}
+					fillColor={'red'}
+					fillOpacity={parseInt(county.properties.CNTY_FIPS) / 200}
+					color={'red'}
+					weight="1"
+				>
+					<ToolTip sticky={true}>
+						{county.properties.NAME}
+					</ToolTip>
+				</GeoJson>
+			{/each}
+		</TileLayer>
 	</Leaflet>
 </div>
